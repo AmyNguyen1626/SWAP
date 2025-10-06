1c20966a90ec89cf1c5d3cf59fde000ab1d7303d
Amy Nguyen <thianhminh.nguyen@students.mq.edu.au>
2025-10-06 22:47:02 +1100

    add

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -9,8 +9,8 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
-}
+};
 
 // Initialise Firebase
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
