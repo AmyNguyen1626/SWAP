60e742f8801918cef346347d6758886cbd1d35fc
charlenegramos <charlene.ramos@students.mq.edu.au>
2025-10-31 14:06:41 +1100

    front end upgrades (homepage and search bar fixes)

--- frontend/src/components/ListingCard.jsx
+++ frontend/src/components/ListingCard.jsx
@@ -18,4 +18,4 @@ export default function ListingCard({ id, image, name, condition, location, pric
       </div>
     </div>
   );
-}
+} 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ import "swiper/css/effect-coverflow";
 export default function Home() {
     const mockListings = [
         {
-            image: "/assets/ford-mustang.png",
+            image: "/assets/ford-coupe.png",
             name: "Ford Mustang Coupe",
             condition: "Very Good",
             location: "Melbourne, VIC",
@@ -19,35 +19,30 @@ export default function Home() {
             name: "Hyundai Santa Fe",
             condition: "Excellent",
             location: "Brisbane, QLD",
-            price: "32100"
         },
         {
             image: "/assets/jeep-wrangler.png",
             name: "Jeep Wrangler",
             condition: "Good",
             location: "Adelaide, SA",
-            price: "49000"
         },
         {
             image: "/assets/mitsubishi-evo.png",
             name: "Mitsubishi Lancer Evo",
             condition: "Very Good",
             location: "Sydney, NSW",
-            price: "28400"
         },
         {
             image: "/assets/nissan-silvia.png",
             name: "Nissan Silvia S15",
             condition: "Excellent",
             location: "Perth, WA",
-            price: "36000"
         },
         {
             image: "/assets/toyota-trueno.png",
             name: "Toyota AE86 Trueno",
             condition: "Fair",
             location: "Canberra, ACT",
-            price: "14000"
         },
     ];
 
